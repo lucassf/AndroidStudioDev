e69c43032d0303c7cb14e508990c894659e13931
Lucas S <lucassoares-2010@hotmail.com>
2022-10-28 23:39:55 -0300

    Added notifications and built first apk

--- app/src/main/java/com/fiapon/helloworld/MainActivity.kt
+++ app/src/main/java/com/fiapon/helloworld/MainActivity.kt
@@ -1,32 +1,37 @@
 package com.fiapon.helloworld
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.*
+import androidx.core.app.NotificationCompat
+import com.fiapon.helloworld.multimedia.GoogleMapsActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val PRIMARY_CHANNEL = "main_channel"
+const val PRIMARY_NOTIFICATION_ID = 100
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var notifyManager: NotificationManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.i("onCreate", "onCreate function")
 
-        val saveGreetButton: Button = findViewById(R.id.btnSaveGreet)
-        saveGreetButton.setOnClickListener {
-            saveGreet()
-        }
+        btnSaveGreet.setOnClickListener { saveGreet() }
+        btnShowGreet.setOnClickListener { showGreet() }
+        btnMultimedia.setOnClickListener { showMultimedia() }
+        btnNotification.setOnClickListener { sendNotification() }
 
-        val showGreetButton: Button = findViewById(R.id.btnShowGreet)
-        showGreetButton.setOnClickListener {
-            showGreet()
-        }
-
-        val multimediaButton: Button = findViewById(R.id.btnMultimedia)
-        multimediaButton.setOnClickListener {
-            showMultimedia()
-        }
+        createNotificationChannel()
     }
 
     private fun saveGreet() {
@@ -50,6 +55,48 @@ class MainActivity : AppCompatActivity() {
     private fun showMultimedia() {
         val intent = Intent(this, MultimediaActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun sendNotification() {
+        val builder = getNotificationBuilder()
+        notifyManager.notify(PRIMARY_NOTIFICATION_ID, builder.build())
+    }
+
+    private fun createNotificationChannel() {
+        notifyManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                PRIMARY_CHANNEL, "basic_notification", NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = "The first notification ever"
+
+            notifyManager.createNotificationChannel(notificationChannel)
+        } else {
+            Toast.makeText(
+                this, "Android version does not support notifications (?)", Toast.LENGTH_SHORT
+            )
+        }
+    }
+
+    private fun getNotificationBuilder(): NotificationCompat.Builder {
+        val notificationIntent = Intent(this, GoogleMapsActivity::class.java)
+
+        val builder = NotificationCompat.Builder(this, PRIMARY_CHANNEL)
+        builder.setContentTitle("You just won 0.011ts!!")
+        builder.setContentText("Begone thot\nWelcome to special skeleton challenge week")
+        builder.setSmallIcon(R.drawable.ic_stat_name)
+        builder.priority = NotificationCompat.PRIORITY_HIGH
+        builder.setContentIntent(
+            PendingIntent.getActivity(
+                this, 0, notificationIntent, 0
+            )
+        )
+
+        return builder
     }
 
     override fun onStart() {
